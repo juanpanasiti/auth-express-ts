715c6d65a63739cad90cf41cc8e68b483269cb28
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2022-07-13 01:20:10 -0300

    New controllet function to get one user by id

--- controllers/user.controllers.ts
+++ controllers/user.controllers.ts
@@ -10,7 +10,7 @@ import * as userServices from '../services/user.services';
 
 export const getAllPaginated = async (req: Request<{}, {}, {}, PaginationQuery>, res: Response) => {
     const { skip = 0, limit = 10 } = req.query;
-
+    
     try {
         const filterOptions: UsersFilterOptions = {
             filter: { $or: [{ status: Status.ACTIVE }, { status: Status.PENDING }] },
@@ -20,7 +20,7 @@ export const getAllPaginated = async (req: Request<{}, {}, {}, PaginationQuery>,
             limit,
             skip,
         };
-        filterOptions.projection = 'username email img role'
+        filterOptions.projection = 'username email img role';
         const userProm = userServices.getManyUsersByFilter(filterOptions);
 
         const [users, total] = await Promise.all([userProm, countProm]);
@@ -38,4 +38,40 @@ export const getAllPaginated = async (req: Request<{}, {}, {}, PaginationQuery>,
             errors: [err],
         });
     }
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+    const uid = req.params.id
+    
+    try {
+        const filterOptions: UsersFilterOptions = {
+            filter: { _id: uid, status: Status.ACTIVE },
+        };
+        
+        filterOptions.projection = 'username email img role';
+        const user = await userServices.getOneUserByFilter(filterOptions);
+
+        if (!user) {
+            return res.status(404).json({errors:[`User ${uid} not found.`]})
+        }
+
+
+        res.status(200).json({
+            response_data: user,
+            errors: [],
+        });
+    } catch (err) {
+        Logger.error(`${err}`);
+        res.status(500).json({
+            errors: [err],
+        });
+    }
+};
+
+export const updateUserById = async (req: Request, res: Response) => {
+    return res.status(501).json('not implemented.. yet');
+};
+
+export const deleteUserById = async (req: Request, res: Response) => {
+    return res.status(501).json('not implemented.. yet');
 };
